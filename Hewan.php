cf3a18ef42c66b6ca4a24954713b5ca3009a477e
windy <71083075+windyoc28@users.noreply.github.com>
2020-09-11 02:12:36 +0700

    percobaan 1

--- Hewan.php
+++ Hewan.php
@@ -2,6 +2,14 @@
 
 include 'Hewan.php';
 
+class Kucing {
+	public $jumlah_kaki, $bisa_terbang, $bersuara;
+
+	public function jumlahKaki(){
+		return $this->jumlah_kaki;
+	}
+}
+
 $moci = new Kucing;
 $moci->jumlah_kaki = 4;
 echo "Moci adalah kucing <br>";
@@ -9,29 +17,3 @@ echo "Punya kaki sebanyak : ".$moci->jumlah_kaki."<br>";
 echo $moci->bisa_terbang."<br>";
 echo "Suaranya : ".$moci->bersuara()."<br>";
 
-echo "<hr>";
-
-$dogy = new Anjing;
-$dogy->jumlah_kaki = 4;
-echo "Dogy adalah anjing <br>";
-echo "Punya kaki sebanyak : ".$dogy->jumlah_kaki."<br>";
-echo $dogy->bisa_terbang."<br>";
-echo "Suaranya : ".$dogy->bersuara()."<br>";
-
-echo "<hr>";
-
-$marlen = new Elang;
-$marlen->jumlah_kaki = 2;
-echo "Marlen adalah elang <br>";
-echo "Punya kaki sebanyak : ".$marlen->jumlah_kaki."<br>";
-echo $marlen->bisa_terbang."<br>";
-echo "Suaranya : ".$marlen->bersuara()."<br>";
-
-echo "<hr>";
-
-$swan = new Angsa;
-$swan->jumlah_kaki = 2;
-echo "Swan adalah angsa <br>";
-echo "Punya kaki sebanyak : ".$swan->jumlah_kaki."<br>";
-echo $swan->bisa_terbang."<br>";
-echo "Suaranya : ".$swan->bersuara()."<br>";
